2e107dae2d5f309e9942c966300bb4496a3e911d
win7 <win7.eam@gmail.com>
2023-10-04 22:54:48 -0500

    clean code

--- GNN_Unsupervised/experiments.py
+++ GNN_Unsupervised/experiments.py
@@ -16,9 +16,8 @@ from main.serializers import ExperimentSerializer
 import format_go as fi
 import prepro_go as pp
 import node_edge_go as ne
-""" import processing as pg
-import change_detection as cd
-import processing_biocyc as pb """
+import change_go as ch
+import biocyc_go as by
 
 import time
 
@@ -49,17 +48,11 @@ def main(experiment):
     print("\nNode-Edge embedding")
     ne.main(experiment)
 
-    """ print("\nNode embeddings")
-    dgi.main(experiment)
-
-    print("\nProcessing")
-    pg.main(experiment)
-
     print("\nChange detection") 
-    cd.main(experiment)
+    ch.main(experiment)
 
     print("\nProcessing biocyc")
-    pb.main(experiment)
+    by.main(experiment)
 
     subject, from_email, to = 'Metabolomic Analysis', 'edwin.alvarez@pucp.edu.pe', experiment.email
     text_content = 'This is an important message.'
@@ -67,14 +60,14 @@ def main(experiment):
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     msg.attach_alternative(html_content, "text/html")
 
-    dir = "{}/GNN_Unsupervised/output/{}/biocyc/".format(os.getcwd(), experiment.id)
+    """ dir = "{}/GNN_Unsupervised/output/{}/biocyc/".format(os.getcwd(), experiment.id)
 
     files = os.listdir(dir)
     for item in files:
         aux_dir = "{}{}".format(dir, item)
         with open(aux_dir) as file:
             msg.attach(item, file.read(), "application/csv")
-    msg.send()
+    msg.send() """
 
     end = time.time()
     elapsed_time = round((end - start) / 60, 2)
@@ -82,5 +75,5 @@ def main(experiment):
     # experiment = Experiment.objects.get(pk=experiment.id)
     experiment.runtime = elapsed_time
     experiment.save()
-    print(elapsed_time) """
+    print(elapsed_time)
     print("End")
